cd8c48ba1eabe204f77edd74b83518ee4355e54d
Konstantinos Kaloutas <sprocketc@gmail.com>
2016-01-19 12:57:43 +0200

    Remove isDevelopingAddon

--- index.js
+++ index.js
@@ -4,10 +4,6 @@
 module.exports = {
   name: 'ember-justified-gallery',
 
-  isDevelopingAddon: function() {
-    return true;
-  },
-
   included: function(app) {
     app.import(app.bowerDirectory + '/Justified-Gallery/dist/css/justifiedGallery.css');
     app.import(app.bowerDirectory + '/Justified-Gallery/dist/js/jquery.justifiedGallery.js');
